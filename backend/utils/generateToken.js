c9f0e8e7c2fa03adae6c4c190ace4eea23297390
yashwant mergewar <yashwantmergewar@gmail.com>
2025-12-05 11:57:10 +0530

    user access and refresh token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,30 @@
 const generateAccessToken = (user) => {
-
+    return jwt.sign(
+        {
+            id: user.id,
+            email: user.userEmail,
+            fullname: user.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+        }
+    )
 }
 
 const generateRefreshToken = (user) => {
-    
+    return jwt.sign(
+        {
+            id: user.id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+        }
+    )
+}
+
+export{
+    generateAccessToken,
+    generateRefreshToken
 }
