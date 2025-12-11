593e00fc3a6a207f9217f02fe78a86e0335d0597
yashwant mergewar <yashwantmergewar@gmail.com>
2025-12-11 12:54:27 +0530

    admin routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { loginUser } from "../controllers/user.controller.js";
+import { loginUser, logOutUser, registerUser } from "../controllers/user.controller.js";
 
 
 const router = Router();
 router.route('/login').post(loginUser);
+router.route('/register').post(registerUser);
+router.route('/logout').post(verifyJWT, logOutUser);
 
 export default router;
