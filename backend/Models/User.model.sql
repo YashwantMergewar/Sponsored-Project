8d7ba84ce4a68607d57c8f8ba6e741a90421f379
yashwant mergewar <yashwantmergewar@gmail.com>
2025-12-12 18:42:25 +0530

    Refactor database models: remove Elections and Votes tables, update Users table structure

--- backend/Models/User.model.sql
+++ backend/Models/User.model.sql
@@ -3,12 +3,13 @@ use Sponsored;
 
 create table users(
 user_id int auto_increment primary key,
+fullname varchar(255) not null,
 username varchar(100) not null,
 password_hash varchar(100) not null unique,
 email varchar(100) not null unique,
-role enum('admin' , 'user') not null,
-voter_id int null,
-foreign key(voter_id) references voter(voter_id) on delete set null
+role enum('admin') default "admin" not null,
+refreshToken text,
+createdAt timestamp default current_timestamp
 );
 
  
