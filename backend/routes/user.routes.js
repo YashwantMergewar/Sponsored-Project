efbc9bc35a6e5960a8c92197c96dea6675729bbb
yashwant mergewar <yashwantmergewar@gmail.com>
2025-12-12 12:25:31 +0530

    new routes are added for user

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { loginUser } from "../controllers/user.controller.js";
+import { userProfile, editUserProfile, changeUserPassword } from "../controllers/user.controller.js";
 
 
 const router = Router();
 router.route('/login').post(loginUser);
+router.route('/profile').get(verifyJWT, userProfile)
+router.route('/profile').patch(verifyJWT, editUserProfile);
+router.route('/change-password').patch(verifyJWT, changeUserPassword);
 
 export default router;
