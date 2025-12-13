c1f58de33a192d4551bb50255c38ff21eb1fc83a
yashwant mergewar <yashwantmergewar@gmail.com>
2025-12-13 18:32:34 +0530

    Refactor database models: remove Candidates and Caste tables, update Voter table structure

--- backend/Models/Voter.model.sql
+++ backend/Models/Voter.model.sql
@@ -2,17 +2,17 @@ use Sponsored;
 
 create table voters(
 voter_id int auto_increment primary key,
-full_name varchar(100) not null,
-head_of_family varchar(100),
-mobile_no varchar(15),
-dob date,
-age int,
-caste_id int,
+fullname varchar(100) not null,
+email varchar(100) not null unique,
+head_of_family varchar(100) not null,
+mobile_no varchar(10) not null,
+aadhar_no varchar(12) not null unique,
+dob date not null,
+age int not null,
+religion enum('Hindu' , 'Muslim' , 'Christian' , 'Sikh' , 'Buddhist' , 'Jain' , 'Other') default 'Hindu',
+caste varchar(100) not null,
+category enum('General' , 'OBC' , 'SC' , 'ST' , 'EWS', 'VJNT') default 'General',
 prabhag_no int,
 house_no varchar(20),
-user_id int,
-deceased boolean default 0,
 created_at timestamp default current_timestamp,
-foreign key(caste_id) references castes(caste_id),
-foreign key(user_id) references users(user_id)
 );
